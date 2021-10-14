229b6f8128d12bc7d7041176cc56614689671ce4
Gustavo <gustavomarquesmm@gmail.com>
2021-10-14 16:01:22 -0300

    Manutenção

--- src/App.js
+++ src/App.js
@@ -2,55 +2,57 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HeaderPage from "./components/headerPage";
 import api from "./services/api";
-import Axios from 'axios'
 import { useState, useEffect } from "react";
-import BotaoBuscarPost from "./components/botaoBuscarPost";
+import Axios from "axios";
 
 function App() {
   const [dados, setDados] = useState([]);
-  const [base64, setBase64] = useState();
+  const [cabecalho, setCabecalho] = useState([])
 
-
+  const maisInformacoes = async (pagina) => {
+    await Axios
+    .get("https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518", {params: {page: pagina}})
+    .then((response) => {
+      setDados(response.data)
+      console.log(dados)
+    })
+    .catch((err) => {
+      console.error("ops! ocorreu um erro " + err);
+    });
+  }
+  
   useEffect(() => {
     api
       .get()
       .then((response) => {
         setDados(response.data);
+        setCabecalho(response.headers)
+        console.log(response.data)
       })
       .catch((err) => {
         console.error("ops! ocorreu um erro " + err);
       });
   }, []);
 
-  useEffect(() => {
-    Axios
-      .get(
-        'https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518',
-        {
-          responseType: "arraybuffer",
-        }
-      )
-      .then((response) =>
-        setBase64(Buffer.from(response.data, "binary").toString("base64"))
-      );
-  }, []);
-
-
   return (
     <section>
       <HeaderPage titulo="Blog dos Programadores" />
 
-      <div className="container my-5 d-flex flex-wrap  align-items-stretch">
+      <div className="container my-5 d-flex flex-wrap">
         <div className="g-0 justify-content-around ">
           <div className="px-2 py-3 row  ">
             {dados.map((dado, index) => {
               return (
-                <div className="p-2 col-md-6 col-sm-12 col-lg-4">
-                  <div
-                    key={dado.id}
-                    className="card"
-                  >
-                    <img className="card-image-top" alt="Imagem do Post"/>
+                <div key={index} className="p-2 col-md-6 col-sm-12 col-lg-4">
+                  <div  className="card">
+                    <img
+                      className="card-image-top"
+                      src={
+                        dado._embedded["wp:featuredmedia"][0].media_details
+                          .sizes.medium_large.source_url
+                      }
+                      alt="Imagem do Post"
+                    />
 
                     <div className="card-body">
                       <h4 className="card-title text-center">
@@ -58,12 +60,19 @@ function App() {
                       </h4>
                     </div>
                     <div className="pt-4 d-grid card-footer">
-                      <BotaoBuscarPost  />
+                      <a className="btn btn-primary" role="button" href={dado.link}>
+                        Clique aqui
+                      </a>
                     </div>
                   </div>
                 </div>
               );
             })}
+            <div className="py-5 d-flex justify-content-center">
+              <button onClick={() => maisInformacoes(2)} className="btn btn-info" type="submit">
+                Carregar Mais
+              </button>
+            </div>
           </div>
         </div>
       </div>
